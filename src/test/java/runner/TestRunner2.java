9eed2962e70fe7df7beb28f94e7af17a3f21d492
Thirumal321 <104666716+Thirumal321@users.noreply.github.com>
2025-03-10 17:00:00 +0530

    Update TestRunner2.java

--- src/test/java/runner/TestRunner2.java
+++ src/test/java/runner/TestRunner2.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
 	        features = "src/test/resources/features", 
 	        glue = "stepDefinitions",
-	        plugin = {"pretty", "html:target/cucumber-reports"}
+	        plugin = {"pretty", "html:target/cucumber-reports-html.html"}
 		plugin = {"pretty", "json:target/cucumber-reports-json/Cucumber.json"} // Generate JSON report	 
 	)
 	public class TestRunner2 extends AbstractTestNGCucumberTests {
