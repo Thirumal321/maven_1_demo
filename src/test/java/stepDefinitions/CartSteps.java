db672cd4c5ab017c25e8099832773c4b1e467f72
Thirumal321 <104666716+Thirumal321@users.noreply.github.com>
2025-03-11 09:06:58 +0530

    Update CartSteps.java

--- src/test/java/stepDefinitions/CartSteps.java
+++ src/test/java/stepDefinitions/CartSteps.java
@@ -13,7 +13,7 @@ import java.time.Duration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -22,7 +22,7 @@ public class CartSteps {
 	private WebDriver driver;
 	@Before("@CartTest")
 	public void setup() throws InterruptedException {
-		driver=new EdgeDriver();
+		driver=new ChromeDriver();
 		
 	}
     @Given("I am on the login page")
